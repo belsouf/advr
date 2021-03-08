373382473739e0750af46091d5b8725e682a3f94
belsouf <soufyane.belfkier@gmail.com>
2021-03-08 11:41:53 +0100

    webpack config wip

---
+++ public/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./lib/components/Index.js":
+/*!*********************************!*\
+  !*** ./lib/components/Index.js ***!
+  \*********************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: Cannot find module 'babel-preset-react'\\nRequire stack:\\n- C:\\\\laragon\\\\www\\\\advr\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\files\\\\plugins.js\\n- C:\\\\laragon\\\\www\\\\advr\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\files\\\\index.js\\n- C:\\\\laragon\\\\www\\\\advr\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\index.js\\n- C:\\\\laragon\\\\www\\\\advr\\\\node_modules\\\\babel-loader\\\\lib\\\\index.js\\n- C:\\\\laragon\\\\www\\\\advr\\\\node_modules\\\\loader-runner\\\\lib\\\\loadLoader.js\\n- C:\\\\laragon\\\\www\\\\advr\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js\\n- C:\\\\laragon\\\\www\\\\advr\\\\node_modules\\\\webpack\\\\lib\\\\NormalModule.js\\n- C:\\\\laragon\\\\www\\\\advr\\\\node_modules\\\\webpack\\\\lib\\\\NormalModuleFactory.js\\n- C:\\\\laragon\\\\www\\\\advr\\\\node_modules\\\\webpack\\\\lib\\\\Compiler.js\\n- C:\\\\laragon\\\\www\\\\advr\\\\node_modules\\\\webpack\\\\lib\\\\webpack.js\\n- C:\\\\laragon\\\\www\\\\advr\\\\node_modules\\\\webpack\\\\lib\\\\index.js\\n- C:\\\\laragon\\\\www\\\\advr\\\\node_modules\\\\webpack-cli\\\\lib\\\\webpack-cli.js\\n- C:\\\\laragon\\\\www\\\\advr\\\\node_modules\\\\webpack-cli\\\\lib\\\\bootstrap.js\\n- C:\\\\laragon\\\\www\\\\advr\\\\node_modules\\\\webpack-cli\\\\bin\\\\cli.js\\n- C:\\\\laragon\\\\www\\\\advr\\\\node_modules\\\\webpack\\\\bin\\\\webpack.js\\n- If you want to resolve \\\"react\\\", use \\\"module:react\\\"\\n- Did you mean \\\"@babel/react\\\"?\\n    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:625:15)\\n    at resolve (C:\\\\laragon\\\\www\\\\advr\\\\node_modules\\\\v8-compile-cache\\\\v8-compile-cache.js:164:23)\\n    at resolveStandardizedName (C:\\\\laragon\\\\www\\\\advr\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\files\\\\plugins.js:100:7)\\n    at resolvePreset (C:\\\\laragon\\\\www\\\\advr\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\files\\\\plugins.js:48:10)\\n    at loadPreset (C:\\\\laragon\\\\www\\\\advr\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\files\\\\plugins.js:67:20)\\n    at createDescriptor (C:\\\\laragon\\\\www\\\\advr\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-descriptors.js:154:9)\\n    at C:\\\\laragon\\\\www\\\\advr\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-descriptors.js:109:50\\n    at Array.map (<anonymous>)\\n    at createDescriptors (C:\\\\laragon\\\\www\\\\advr\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-descriptors.js:109:29)\\n    at createPresetDescriptors (C:\\\\laragon\\\\www\\\\advr\\\\node_modules\\\\@babel\\\\core\\\\lib\\\\config\\\\config-descriptors.js:101:10)\");\n\n//# sourceURL=webpack://advr/./lib/components/Index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./lib/components/Index.js"]();
+/******/ 	
+/******/ })()
+;
